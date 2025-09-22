037d44874dcc653a1c7b5cebabdeb955eb7f5050
pavel-miniutka <pavel.miniutka@gmail.com>
2025-09-22 15:05:07 +0300

    test 17

--- server/src/main/java/lsfusion/server/logics/action/change/AddObjectAction.java
+++ server/src/main/java/lsfusion/server/logics/action/change/AddObjectAction.java
@@ -79,6 +79,7 @@ public class AddObjectAction<T extends PropertyInterface, I extends PropertyInte
 
     
     
+    
     // not sure that we gonna support this branch
     @Deprecated
     protected boolean needDialog() {
